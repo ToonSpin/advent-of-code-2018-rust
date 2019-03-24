0e9a6ab2e6784ab04d5f6abdce629d58f339c7bb
Toon Spin <toon.spin@gmail.com>
2019-03-24 23:29:47 +0100

    Day 05, part 2

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -13,17 +13,9 @@ fn reacts_with(a:&u8, b:&u8) -> bool {
     false
 }
 
-fn main () -> io::Result<()> {
-    let mut input = String::new();
-    io::stdin().lock().read_to_string(&mut input)?;
-    let input = input.trim();
-
-    let units = Vec::from(input.as_bytes());
-    let mut units = VecDeque::from(units);
-
-    let mut done = false;
-    while !done {
-        done = true;
+fn react(mut units: Vec<u8>) -> usize {
+    loop {
+        let mut done = true;
         let mut i = 1;
 
         while i < units.len() {
@@ -35,8 +27,42 @@ fn main () -> io::Result<()> {
                 i += 1;
             }
         }
+
+        if done {
+            return units.len();
+        }
+    }
+}
+
+fn strip_unit(mut units: Vec<u8>, unit: &u8) -> Vec<u8> {
+    let mut i = 0;
+    loop {
+        match units.get(i) {
+            Some(u) => {
+                if *u == *unit || *u == unit + 32 {
+                    units.remove(i);
+                } else {
+                    i += 1;
+                }
+            },
+            None => { return units; },
+        }
+    }
+}
+
+fn main () -> io::Result<()> {
+    let mut input = String::new();
+    io::stdin().lock().read_to_string(&mut input)?;
+    let input = input.trim();
+
+    let units = Vec::from(input.as_bytes());
+
+    println!("Length after reaction: {}", react(Vec::from(units.clone())));
+
+    for unit in "ABCDEFGHIJKLMNOPQRSTUVWXYZ".as_bytes() {
+        let stripped = strip_unit(Vec::from(units.clone()), unit);
+        println!("Length after reaction after stripping {}: {}", *unit as char, react(stripped));
     }
 
-    println!("Length after reaction: {}", units.len());
     Ok(())
 }
